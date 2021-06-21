7dcc168c29de7eb18ef6ea648c4778e77b086aa6
nikita-t5 <nickita.tashirev@yandex.ru>
2021-06-21 15:49:40 +0300

    jar packaging

--- src/main/java/ru/ibs/project/config/Configuration.java
+++ src/main/java/ru/ibs/project/config/Configuration.java
@@ -1,8 +1,15 @@
 package ru.ibs.project.config;
 
 
-@org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
+@org.springframework.context.annotation.Configuration
 public class Configuration {
+
+    @Bean
+    RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
 
 }
--- src/main/java/ru/ibs/project/entities/Vacancy.java
+++ src/main/java/ru/ibs/project/entities/Vacancy.java
@@ -1,10 +1,13 @@
 package ru.ibs.project.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 //@Entity
+
+@JsonIgnoreProperties (ignoreUnknown = true) //любые поля, не связанные с полями класса, должны быть проигнорированы.
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
